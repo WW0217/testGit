72ccbd504e7ead42acfe7e2a042d8f7a8f7ec819
WW0217 <1993658107@qq.com>
2021-09-21 11:19:34 +0800

    创建主干

---
+++ HelloGit/src/main/java/HelloGit.java
@@ -0,0 +1,9 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit!");
+    }
+
+}
